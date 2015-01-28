d364c2c76ed7d3c70a42df0324bf669d90119f75
Kevin Webber <kevin.webber@gmail.com>
2015-01-28 11:36:43 -0800

    minor API renaming

--- app/actors/game/GameEngineActor.scala
+++ app/actors/game/GameEngineActor.scala
@@ -14,32 +14,25 @@ object GameEngineActor {
 
 class GameEngineActor extends Actor {
   def receive = {
-    case r: RegisterPlayerRequest => handleNewPlayer(r)
+    case r: RegisterPlayerRequest => registerPlayerForGame(r)
   }
 
-  private def handleNewPlayer(r: RegisterPlayerRequest) {
-    System.out.println("finding a game for a new player...")
+  private def registerPlayerForGame(r: RegisterPlayerRequest) {
     findOpenGame(r) match {
-      case Some(game) => {
-        game ! StartGameRequest()
-      }
-      case None => {
-        createNewGame(r)
-      }
+      case Some(game) => game ! StartGameRequest()
+      case None => createNewGame(r)
     }
   }
 
   private def findOpenGame(request: RegisterPlayerRequest): Option[ActorRef] = {
-    System.out.println("finding an open game...")
     for (child <- context.children) {
       val response = attemptRegistration(child, request)
       response.status match {
         case RegisterPlayerResponse.STATUS_GAME_FULL => {} // do nothing, keep looking for an empty game
         case RegisterPlayerResponse.STATUS_OK => {
           response.playerLetter match {
-            case Some(letter) => {
-              return Some(response.game)
-            }
+            case Some(letter) => return Some(response.game)
+            case _ => {} // keep looking
           }
         }
       }
@@ -48,15 +41,12 @@ class GameEngineActor extends Actor {
   }
 
   private def createNewGame(request: RegisterPlayerRequest): ActorRef = {
-    System.out.println("creating a new game...")
     val gameUuid = java.util.UUID.randomUUID.toString
     val newGame = context.actorOf(Props[GameActor], name = "gameActor" + gameUuid)
     attemptRegistration(newGame, request).game
   }
 
   private def attemptRegistration(game: ActorRef, request: RegisterPlayerRequest): RegisterPlayerResponse = {
-    System.out.println("attempting registration...")
-    System.out.println("actor type: " + game.toString())
     implicit val timeout = Timeout(3 seconds)
     val future = game ? request
     Await.result(future, timeout.duration).asInstanceOf[RegisterPlayerResponse]
