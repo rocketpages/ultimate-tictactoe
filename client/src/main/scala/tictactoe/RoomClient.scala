c0e90afcd883943575e7cdfbfd571c3423a9d0db
Kevin Webber <kevin@kevinwebber.ca>
2016-04-06 00:23:25 -0400

    Minor graphical fixes

--- client/src/main/scala/tictactoe/RoomClient.scala
+++ client/src/main/scala/tictactoe/RoomClient.scala
@@ -3,7 +3,7 @@ package tictactoe
 import org.scalajs.dom
 import org.scalajs.dom.raw.{MessageEvent, WebSocket}
 import shared.MessageKeyConstants
-import shared.ServerToClientProtocol.{GameStartedEvent, GameCreatedEvent, ServerToClientWrapper}
+import shared.ServerToClientProtocol.{Payload, GameStartedEvent, GameCreatedEvent, ServerToClientWrapper}
 import upickle.default._
 
 import scala.scalajs.js
@@ -35,42 +35,52 @@ object RoomClient extends js.JSApp {
 
       wrapper.t.toString match {
         case MessageKeyConstants.MESSAGE_HANDSHAKE => {}
-        case MessageKeyConstants.MESSAGE_NEW_GAME_CREATED_EVENT => {
-          val pl = read[GameCreatedEvent](payload)
-          val xName: String = pl.x
-
-          val elem =
-            tr(id:="game-" + pl.uuid,
-              td(p(xName)),
-              td(raw("<form action=\"/game/join\" method=\"POST\" class=\"uk-form\">" +
-                  "<fieldset data-uk-margin>" +
-                    "<input type=\"text\" name=\"nameO\" id=\"nameO\" placeholder=\"Your name\">" +
-                    "<input type=\"hidden\" name=\"nameX\" value=\"" + xName + "\">" +
-                    "<input type=\"hidden\" name=\"uuid\" value=\"" + pl.uuid + "\">" +
-                    "<button class=\"uk-button\">Join game!</button>" +
-                  "</fieldset>" +
-                "</form>")
-              )
-            ).render
-
-          jQuery("#gameList").append(elem)
-        }
-        case MessageKeyConstants.MESSAGE_GAME_STARTED_EVENT => {
-          val pl = read[GameStartedEvent](payload)
-          val xName: String = pl.x
-          val oName: String = pl.o
-
-          val elem =
-            tr(id:="game-" + pl.uuid,
-              td(p(xName)),
-              td(p(oName))
-            ).render
-
-          jQuery("#game-" + pl.uuid).remove()
-          jQuery("#gameList").append(elem)
-        }
+        case MessageKeyConstants.MESSAGE_NEW_GAME_CREATED_EVENT => handleGameCreated(payload)
+        case MessageKeyConstants.MESSAGE_GAME_STARTED_EVENT => handleGameStarted(payload)
       }
     }
+  }
+
+  private def handleGameCreated(payload: String): Unit = {
+    val pl = read[GameCreatedEvent](payload)
+    val xName: String = pl.x
+
+    jQuery("#gameList").show()
+    jQuery("#gameListHeader").show()
+
+    val elem =
+    tr(id:="game-" + pl.uuid,
+      td(p(xName)),
+      td(raw("<form action=\"/game/join\" method=\"POST\" class=\"uk-form\">" +
+        "<fieldset data-uk-margin>" +
+        "<input type=\"text\" name=\"nameO\" id=\"nameO\" placeholder=\"Your name\">" +
+        "<input type=\"hidden\" name=\"nameX\" value=\"" + xName + "\">" +
+        "<input type=\"hidden\" name=\"uuid\" value=\"" + pl.uuid + "\">" +
+        "<button class=\"uk-button\">Join game!</button>" +
+        "</fieldset>" +
+        "</form>")
+      )
+    ).render
+
+    jQuery("#gameList").append(elem)
+  }
+
+  private def handleGameStarted(payload: String): Unit = {
+    val pl = read[GameStartedEvent](payload)
+    val xName: String = pl.x
+    val oName: String = pl.o
+
+    jQuery("#gameList").show()
+    jQuery("#gameListHeader").show()
+
+    val elem =
+      tr(id:="game-" + pl.uuid,
+        td(p(xName)),
+        td(p(oName))
+      ).render
+
+    jQuery("#game-" + pl.uuid).remove()
+    jQuery("#gameList").append(elem)
   }
 
 }
